620d2a59458fe2093bcbab68144d24f11446bd51
Archisman Banerjee <archisman.tech@gmail.com>
2018-12-08 14:33:55 +0530

    Initial Setup

---
+++ MyBliss/Controller/Launch/LaunchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchViewController.swift
+//  MyBliss
+//
+//  Created by Archisman Banerjee on 08/12/18.
+//  Copyright © 2018 Archisman Banerjee. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
